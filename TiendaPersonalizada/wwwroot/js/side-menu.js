22e27a6a7a74a91f26f2d76bc4a24b7d0d660e46
TermiSenpai <escarpaprieto@gmail.com>
2024-12-09 07:09:41 +0100

    dark web front

---
+++ TiendaPersonalizada/wwwroot/js/side-menu.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("sideMenu").classList.add("open");
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").classList.remove("open");
+}
